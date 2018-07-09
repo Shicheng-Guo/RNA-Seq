f568e96476887fd4556f0defd6aa68daad912b55
Sachin Pundhir <pundhir@lobster.bric.dk>
2018-07-09 10:47:06 +0200

    Update Mon Jul  9 10:46:56 CEST 2018

--- mapStat.sh
+++ mapStat.sh
@@ -7,7 +7,7 @@ usage() {
 	echo Author: BRIC, University of Copenhagen, Denmark
 	echo Version: 1.0
 	echo Contact: pundhir@binf.ku.dk
-	echo "Usage: mapStat -s <file> -q <file> -f <file>"
+	echo "Usage: mapStat.sh -s <file> -q <file> -f <file>"
 	echo "Options:"
 	echo " -s <file>   [input mapStat file from mapSEReads.sh]"
 	echo "[optional]"
@@ -23,12 +23,12 @@ usage() {
 MAPPING_FREQUENCY=1
 
 #### parse options ####
-while getopts s:q:f:u:h ARG; do
+while getopts s:q:f:m:h ARG; do
 	case "$ARG" in
 		s) MAPSTATFILE=$OPTARG;;
 		q) RAWFASTQFILE=$OPTARG;;
 		f) CLIPPEDFASTQFILE=$OPTARG;;
-        u) BAMFILE=$OPTARG;;
+        m) BAMFILE=$OPTARG;;
 		h) HELP=1;;
 	esac
 done
@@ -55,8 +55,8 @@ if [ ! -z "$CLIPPED_READS_COUNT" -a -f "$CLIPPEDFASTQFILE" ]; then
 fi
 
 ## tabulate mapping statistics
-echo -e "#reads (raw)\t#reads (after qualityCheck)\t#reads (for mapping)\t# reads (unpaired)\t#reads (unmapped)\t#reads (aligned 1 time)\t#reads (aligned >1 time)\talignment rate"
-zless $MAPSTATFILE | perl -ane 'BEGIN { print "'$RAW_READS_COUNT'\t'$CLIPPED_READS_COUNT' ('$PER')"; } if($_=~/^[0-9\s]+/) { $_=~s/\;.*//g; chomp($_); $_=~s/\s+[a-zA-Z]+.*//g; print "\t$_"; } END { print "\n"; }'
+echo -e "fastq_file\t#reads (raw)\t#reads (after qualityCheck)\t#reads (for mapping)\t# reads (unpaired)\t#reads (unmapped)\t#reads (aligned 1 time)\t#reads (aligned >1 time)\talignment rate"
+zless $MAPSTATFILE | perl -ane 'BEGIN { print "'$RAWFASTQFILE'\t'$RAW_READS_COUNT'\t'$CLIPPED_READS_COUNT' ('$PER')"; } if($_=~/^[0-9\s]+/) { $_=~s/\;.*//g; chomp($_); $_=~s/\s+[a-zA-Z]+.*//g; print "\t$_"; } END { print "\n"; }'
 
 if [ ! -z "$BAMFILE" ]; then
     ID=`echo $BAMFILE | perl -an -F'/\,/' -e '$ID=(); foreach(@F) { $_=~s/^.+\///g; $_=~s/\..+$//g; chomp($_); $ID.=$_."_"; } $ID=~s/\_$//g; print "$ID\n";' | perl -an -F'//' -e 'chomp($_); if(scalar(@F)>50) { $_=~s/\_R[0-9]+.*$//g; print "$_\n"; } else { print "$_\n"; }'`;
